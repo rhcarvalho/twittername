405bbef0bf61c876a46bf7392c9d6dfde6841983
Rodolfo Carvalho <rhcarvalho@gmail.com>
2010-11-20 23:33:19 -0200

    Cooperative multithreaded implementation

--- mytwittername.py
+++ mytwittername.py
@@ -1,3 +1,6 @@
+from Queue import Queue
+from threading import Thread
+from time import time as now
 from urllib2 import urlopen
 from string import ascii_lowercase, digits
 from itertools import chain, product, ifilter, islice
@@ -28,15 +31,30 @@ def gen_names(start, end):
 
 def vnames(start, end):
     return ifilter(valid, gen_names(start, end))
+    
+def check_availability():
+    while True:
+        username = q.get()
+        if valid(username):
+            print username
+        q.task_done()
 
 if __name__ == "__main__":
+    t0 = now()
     try:
-        #print list(islice(vnames(2, 3), 1)) or "nothing found"
-        for username in vnames(2, 3):
-            print username
-        else:
-            print " - the end -"
+        q = Queue()
+        num_worker_threads = 20
+        for i in xrange(num_worker_threads):
+             t = Thread(target=check_availability)
+             t.daemon = True
+             t.start()
+        
+        for name in gen_names(2, 3):
+            q.put(name)
+        
+        q.join()
     finally:
+        print "All done in %.2f seconds." % (now() - t0)
         cachef = open("cache", "w")
         try:
             json.dump(_cache, cachef)
