341b8ece05c3e5f540159b1dc6d00483f8100a59
Rodolfo Carvalho <rhcarvalho@gmail.com>
2010-11-21 02:43:55 -0200

    Better behaved threads that will recover to work after exceptions.

--- mytwittername.py
+++ mytwittername.py
@@ -1,9 +1,10 @@
-from Queue import Queue
+from Queue import Queue, PriorityQueue
 from threading import Thread
 from time import time as now
 from urllib2 import urlopen
 from string import ascii_lowercase, digits
-from itertools import chain, product, ifilter, islice
+from itertools import chain, product
+from functools import partial
 import json
 
 try:
@@ -28,31 +29,45 @@ def valid(username, token="011b6b129568e20eb9e612c821f719b72775e89d", cache=_cac
 def gen_names(start, end):
     valid_chars = ascii_lowercase + "_" + digits
     return chain(*(map("".join, product(valid_chars, repeat=i)) for i in xrange(start, end + 1)))
-
-def vnames(start, end):
-    return ifilter(valid, gen_names(start, end))
     
-def check_availability():
+def check_availability(names_queue, available_names_queue):
     while True:
-        username = q.get()
-        if valid(username):
-            print username
-        q.task_done()
+        username = names_queue.get()
+        try:
+            if valid(username):
+                available_names_queue.put(username)
+        except Exception, e:
+            # I didn't do my job, so I let someone else do it
+            available_names_queue.put("[Exception trying to process '%s': %s]" % (username, e.msg))
+            names_queue.put(username)
+        finally:
+            names_queue.task_done()
+        
+def print_available_names(available_names_queue):
+    while True:
+        username = available_names_queue.get()
+        print username
+        available_names_queue.task_done()
 
 if __name__ == "__main__":
     t0 = now()
     try:
-        q = Queue()
-        num_worker_threads = 20
+        names_queue = PriorityQueue()
+        available_names_queue = Queue()
+        num_worker_threads = 40
         for i in xrange(num_worker_threads):
-             t = Thread(target=check_availability)
+             t = Thread(target=partial(check_availability, names_queue, available_names_queue))
              t.daemon = True
              t.start()
+        t = Thread(target=partial(print_available_names, available_names_queue))
+        t.daemon = True
+        t.start()
         
         for name in gen_names(2, 3):
-            q.put(name)
+            names_queue.put(name)
         
-        q.join()
+        names_queue.join()
+        available_names_queue.join()
     finally:
         print "All done in %.2f seconds." % (now() - t0)
         cachef = open("cache", "w")
