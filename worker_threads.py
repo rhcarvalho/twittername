ecdbe3705999ca22a64391f2e59531344cfed4e7
Rodolfo Carvalho <rhcarvalho@gmail.com>
2010-11-21 00:37:40 -0200

    Playing with self-recorverable threads...

--- worker_threads.py
+++ worker_threads.py
@@ -1,5 +1,5 @@
-from Queue import Queue
-from threading import Thread
+from Queue import PriorityQueue
+from threading import Thread, active_count
 from time import sleep, time as now
 from random import uniform
 
@@ -9,14 +9,22 @@ def worker(id):
     def workfunc():
         while True:
             item = q.get()
-            t = uniform(1, 5)
-            sleep(t)
-            print "Thread <%2s> processed item <%2s> in %.2f seconds." % (id, item, t)
-            q.task_done()
+            try:
+                t = uniform(1, 3)
+                sleep(t)
+                if item == 7:
+                    print "[Thread %s] I cannot process item %s." % (id, item)
+                    raise Exception
+                print "Thread <%2s> processed item <%2s> in %.2f seconds. Active threads: %d." % (id, item, t, active_count())
+            except:
+                # I didn't do my job, so I let someone else do it
+                q.put(item + 0.1)
+            finally:
+                q.task_done()
     return workfunc
 
-q = Queue()
-num_worker_threads = 2
+q = PriorityQueue()
+num_worker_threads = 5
 for i in range(num_worker_threads):
      t = Thread(target=worker(i))
      t.daemon = True
